8402c193e297fc5577f8323a0add8588cfa7d503
weihao <whxtime2022@163.com>
2026-03-15 22:34:53 +0800

    ``` feat(exp): 新增基础实验类实现
    新增Exp_basic基类，提供模型初始化、设备获取、数据加载器、验证、训练和测试等
    基础方法框架。支持多种RNN模型类型（BpttLSTM/GRU、SpttLSTM/GRU及其End变体），
    并集成GPU（CUDA/MPS）和CPU设备选择功能。
    ```

---
+++ exp/exp_basic.py
@@ -0,0 +1,51 @@
+import torch
+import os
+from models import BpttGRU, BpttLSTM, SpttGRU, SpttLSTM, SpttGRU_End, SpttLSTM_End
+
+
+class Exp_basic:
+    def __init__(self, args):
+        self.args = args
+        self.model_dict = {
+            'BpttLSTM': BpttLSTM,
+            'BpttGRU': BpttGRU,
+            'SpttLSTM': SpttLSTM,
+            'SpttGRU': SpttGRU,
+            'SpttLSTM_End': SpttLSTM_End,
+            'SpttGRU_End': SpttGRU_End,
+        }
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+        
+    def _build_model(self):
+        """Subclasses must implement this method"""
+        raise NotImplementedError
+    
+    def _acquire_device(self):
+        if self.args.use_gpu and self.args.gpu_type == 'cuda':
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(
+                self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+            device = torch.device('cuda:{}'.format(self.args.gpu))
+            print('Use GPU: cuda:{}'.format(self.args.gpu))
+        elif self.args.use_gpu and self.args.gpu_type == 'mps':
+            device = torch.device('mps')
+            print('Use GPU: mps')
+        else:
+            device = torch.device('cpu')
+            print('Use CPU')
+        return device
+    
+    def _get_loader(self):
+        raise NotImplementedError
+    
+    def validate(self):
+        raise NotImplementedError
+    
+    def train(self):
+        raise NotImplementedError
+    
+    def test(self):
+        raise NotImplementedError
+    
+    
+    
