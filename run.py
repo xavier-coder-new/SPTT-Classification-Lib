4967ed6d4f5283afbd6f8f1ccd329e5e960ec81a
weihao <whxtime2022@163.com>
2026-03-18 20:18:25 +0800

    ``` feat(run.py): 添加滑动窗口参数支持
    新增了 --slide_window_nums 参数用于控制 SPTT 的滑动窗口数量，
    默认值为 4，以支持更灵活的数据处理配置。
    ```

--- run.py
+++ run.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
     parser.add_argument("--num_layers", type=int, default=1, help="number of layers for RNN model",)
     parser.add_argument("--use_multi_gpu", action="store_true", help="whether to use multiple GPUs")
     parser.add_argument("--num_worker", type=int, default=4, help="number of workers for data loading")
+    parser.add_argument("--slide_window_nums", type=int, default=4, help="number of sliding windows for SPTT")
     args = parser.parse_args()
 
     global_vars.krank = args.krank
