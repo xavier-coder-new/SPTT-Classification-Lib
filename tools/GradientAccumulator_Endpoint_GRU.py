87b7e34c0971650e9a84deeadf139dc085abfcc1
weihao <whxtime2022@163.com>
2026-03-19 19:20:56 +0800

    ``` refactor(tools): 重命名SBTPCA参数方法为SPTT参数方法
    - 将save_sbtpca_parameter方法重命名为save_sptt_parameter
    - 将deliver_sbtpca_parameter方法重命名为deliver_sptt_parameter
    - 保持方法功能和参数不变，仅更新方法名称以匹配正确的缩写
    ```

--- tools/GradientAccumulator_Endpoint_GRU.py
+++ tools/GradientAccumulator_Endpoint_GRU.py
@@ -22,7 +22,7 @@ class GradientAccumulatorEndpointGRU:
     def get_concatenated_gradients(self):
         return self.input, self.hx, self.delta_ih, self.delta_hh
 
-    def save_sbtpca_parameter(
+    def save_sptt_parameter(
         self,
         X_matrix_ih, Sigma_ih, Sigma_matrix_ih, Delta_matrix_ih,
         X_matrix_hh, Sigma_hh, Sigma_matrix_hh, Delta_matrix_hh,
@@ -37,7 +37,7 @@ class GradientAccumulatorEndpointGRU:
         self.Sigma_matrix_hh = Sigma_matrix_hh
         self.Delta_matrix_hh = Delta_matrix_hh
 
-    def deliver_sbtpca_parameter(self):
+    def deliver_sptt_parameter(self):
         return (
             self.X_matrix_ih, self.Sigma_ih, self.Sigma_matrix_ih, self.Delta_matrix_ih,
             self.X_matrix_hh, self.Sigma_hh, self.Sigma_matrix_hh, self.Delta_matrix_hh,
