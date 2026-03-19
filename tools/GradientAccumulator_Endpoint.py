102b8b062c1be76d5a23ab1381fbf8439b56a34c
weihao <whxtime2022@163.com>
2026-03-19 19:20:03 +0800

    ``` refactor(tools): 重构GradientAccumulator_Endpoint类
    - 将类名从GradientAccumulator_hidden重命名为GradientAccumulatorEndpointLSTM
    - 移除对global_param模块的依赖
    - 修改内部数据结构，将列表存储改为直接存储单个值
    - 更新get_concatenated_gradients方法，直接返回存储的值而非拼接结果
    - 格式化save_sbtpca_parameter方法参数以提高可读性
    ```

--- tools/GradientAccumulator_Endpoint.py
+++ tools/GradientAccumulator_Endpoint.py
@@ -1,48 +1,40 @@
 import torch
-from global_param import global_vars
 
-class GradientAccumulator_hidden:
+
+class GradientAccumulatorEndpointLSTM:
     def __init__(self):
         self.reset()
-    
+
     def reset(self):
-        self.input = []
-        self.hx = []
-        self.delta = []
-        self.length = 0
-        # print("执行了sbtpca的reset")
-    
+        self.input = None
+        self.hx = None
+        self.delta = None
+
     def accumulate(self, input, hx, delta):
-        self.input.append(input)
-        self.hx.append(hx)
-        self.delta.append(delta)
-        # self.length += len(input)
-        # if self.length >= global_vars.total_time_block_size:
-        #     global_vars.compression_finished = True
-        global_vars.compression_finished = True
-        
+        self.input = input
+        self.hx = hx
+        self.delta = delta
+
     def get_concatenated_gradients(self):
-        return torch.cat(self.input, dim=0), torch.cat(self.hx, dim=0), torch.cat(self.delta, dim=0)
-    
-    def save_sbtpca_parameter(self, X_matrix_ih, Sigma_ih, Sigma_matrix_ih, Delta_matrix_ih,
-                              X_matrix_hh, Sigma_hh, Sigma_matrix_hh, Delta_matrix_hh):
-        
+        return self.input, self.hx, self.delta
+
+    def save_sbtpca_parameter(
+        self,
+        X_matrix_ih, Sigma_ih, Sigma_matrix_ih, Delta_matrix_ih,
+        X_matrix_hh, Sigma_hh, Sigma_matrix_hh, Delta_matrix_hh,
+    ):
         self.X_matrix_ih = X_matrix_ih
         self.Sigma_ih = Sigma_ih
         self.Sigma_matrix_ih = Sigma_matrix_ih
         self.Delta_matrix_ih = Delta_matrix_ih
-        
+
         self.X_matrix_hh = X_matrix_hh
         self.Sigma_hh = Sigma_hh
         self.Sigma_matrix_hh = Sigma_matrix_hh
         self.Delta_matrix_hh = Delta_matrix_hh
-        
+
     def deliver_sbtpca_parameter(self):
-        return self.X_matrix_ih, self.Sigma_ih, self.Sigma_matrix_ih, self.Delta_matrix_ih, \
-               self.X_matrix_hh, self.Sigma_hh, self.Sigma_matrix_hh, self.Delta_matrix_hh
-    
-    def save_time_backward(self, time_backward):
-        self.time_back_ward = time_backward
-    
-    def deliver_time_backward(self):
-        return self.time_back_ward
+        return (
+            self.X_matrix_ih, self.Sigma_ih, self.Sigma_matrix_ih, self.Delta_matrix_ih,
+            self.X_matrix_hh, self.Sigma_hh, self.Sigma_matrix_hh, self.Delta_matrix_hh,
+        )
